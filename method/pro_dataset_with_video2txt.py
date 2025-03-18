4fd3cf7fd9e16442c580b457505472063f250217
unknown <you@example.com>
2025-03-18 19:46:08 +0800

    test

--- method/pro_dataset_with_video2txt.py
+++ method/pro_dataset_with_video2txt.py
@@ -1,14 +1,23 @@
 import os
 import video2txt
-
+vidlist = []
+txtlist = []
 root_path = '../SLR_dataset/color'
 text_path = '../SLR_dataset/seq_txt'
 dir_list = os.listdir(root_path)
 file_list = os.listdir(os.path.join(root_path,dir_list[1]))
 print(os.path.join(root_path,dir_list[1],dir_list[1]))
 
-for i in dir_list:
+for i in dir_list[2:]:
     file_list = os.listdir(os.path.join(root_path, i))
     for j in file_list:
-        print(os.path.join(root_path, i, j))
-        video2txt.video2txt(os.path.join(root_path, i, j), os.path.join(text_path, i, j))
+        vidlist.append(os.path.join(root_path, i, j))
+        txtlist.append(os.path.join(text_path, i, j))
+
+def go(start_data):
+    for i in range(len(vidlist)):
+        if i >= start_data:
+            video2txt.video2txt(vidlist[i], txtlist[i])
+        print(i,vidlist[i])
+
+go(0)
